af8ae593fadfa5a7b3c12519b179b67056069020
Trofis <porto.thomas@gmail.com>
2022-07-24 22:20:58 +0200

    fix(dev): list snap component from app-snap-list-component and no more from app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,37 +10,9 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   ngOnInit(): void {
 
-    const mySnap:FaceSnap = {
-      title: "Trofis",
-      description: "My hero !",
-      createdDate: new Date(),
-      snaps: 5,
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
-    }
 
-    const mySnap1:FaceSnap = {
-      title: "Leverbeux",
-      description: "My boy 1",
-      createdDate: new Date(),
-      snaps: 10,
-      imageUrl: 'https://media.gettyimages.com/photos/mallard-duck-walking-in-ice-picture-id155358150?s=612x612'
-    }
-
-    const mySnap2:FaceSnap= {
-      title: "Totos",
-      description: "My boy2",
-      createdDate: new Date(),
-      snaps: 2,
-      imageUrl: 'https://resize.prod.docfr.doc-media.fr/s/1200/img/var/doctissimo/storage/images/fr/www/animaux/diaporamas/ratons-laveurs/2271842-1-fre-FR/Top-10-des-ratons-laveurs-les-plus-adorables.jpg'
-    }
-
-    this.mySnaps = [mySnap, mySnap1, mySnap2];
   }
 
-  mySnaps!: FaceSnap[];
-  // mySnap!: FaceSnap;
-  // mySnap1!: FaceSnap;
-  // mySnap2!: FaceSnap;
 
 
 
--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FaceSnap } from "../model/face-snap-model";
+
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
+
 export class FaceSnapListComponent implements OnInit {
+  faceSnaps!: FaceSnap[];
 
-  constructor() { }
+  ngOnInit() {
+    const mySnap:FaceSnap = {
+      title: "Trofis",
+      description: "My hero !",
+      createdDate: new Date(),
+      snaps: 5,
+      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+    }
 
-  ngOnInit(): void {
+    const mySnap1:FaceSnap = {
+      title: "Leverbeux",
+      description: "My boy 1",
+      createdDate: new Date(),
+      snaps: 10,
+      imageUrl: 'https://media.gettyimages.com/photos/mallard-duck-walking-in-ice-picture-id155358150?s=612x612'
+    }
+
+    const mySnap2:FaceSnap= {
+      title: "Totos",
+      description: "My boy2",
+      createdDate: new Date(),
+      snaps: 2,
+      imageUrl: 'https://resize.prod.docfr.doc-media.fr/s/1200/img/var/doctissimo/storage/images/fr/www/animaux/diaporamas/ratons-laveurs/2271842-1-fre-FR/Top-10-des-ratons-laveurs-les-plus-adorables.jpg'
+    }
+
+    this.faceSnaps = [mySnap, mySnap1, mySnap2];
   }
-
 }
