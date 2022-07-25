54f0c60621ea1b6a6cfe033b2a0c8e33bf73ba39
Trofis <porto.thomas@gmail.com>
2022-07-25 10:10:37 +0200

    feat(dev): routing upgraded + landing page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 
 const routes: Routes = [
-  { path: 'facesnaps', component: FaceSnapListComponent }
+    { path: "", component: LandingPageComponent},
+    { path: 'facesnaps', component: FaceSnapListComponent }
 ];
 
 @NgModule({
