9e7d14b1b4d6295dba315380ab930d4d17b87dae
Trofis <porto.thomas@gmail.com>
2022-07-27 11:22:37 +0200

    fix(dev): removing observable testing (switched on test_observable branch)

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -11,32 +11,16 @@ import { interval, Subject, take, takeUntil, tap } from 'rxjs';
   styleUrls: ['./face-snap-list.component.scss']
 })
 
-export class FaceSnapListComponent implements OnInit, OnDestroy {
+export class FaceSnapListComponent implements OnInit {
   faceSnaps!: FaceSnap[];
-   private destroy$!: Subject<boolean>
 
   constructor(private FaceSnapsService: FaceSnapsService, private router: Router) { }
-  
-   ngOnDestroy(): void {
-    this.destroy$.next(true);
-  }
 
   ngOnInit() {
     this.faceSnaps = this.FaceSnapsService.getAllFaceSnaps();
-
-    this.destroy$ = new Subject<boolean>();
-
-    // interval(1000).pipe(
-    //   take(3),
-    //   tap(console.log)).subscribe();
-
-    interval(1000).pipe(
-      tap(console.log),
-      takeUntil(this.destroy$)).subscribe();
   }
 
   goToSingleFace(faceId: number): void {
     this.router.navigateByUrl('/facesnaps/' + faceId)
-    console.log("router")
   }
 }
